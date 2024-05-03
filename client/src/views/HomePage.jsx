e8c95b47616c6cb66efc8ef5438462b6a6529bc8
SamWatkins67 <118970009+SamWatkins67@users.noreply.github.com>
2024-05-03 02:28:32 -0700

    Update HomePage.jsx

--- client/src/views/HomePage.jsx
+++ client/src/views/HomePage.jsx
@@ -1,7 +1,40 @@
-const HomePage = (props) => {
+import React from "react";
+import TopBanner from "../components/TopBanner";
+import { Link } from "react-router-dom";
+
+
+const HomePage = () => {
     return (
         <>
-            <h1> HomePage Page</h1>
+            <div className="full_screen_background">
+                <TopBanner />
+                <div className="container mt-5">
+                    <h1 className="mb-4">Hike Explorer</h1>
+                    <Link to="/">Go to Dev Page</Link>
+                    <table className="table table-bordered">
+                        <thead>
+                            <tr>
+                                <th scope="col">Hike Name</th>
+                                <th scope="col">Location</th>
+                                <th scope="col">Distance</th>
+                                <th scope="col">Difficulty</th>
+                                <th scope="col">Facilities</th>
+                                <th scope="col">Rating</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td>Example Hike</td>
+                                <td>Example Location</td>
+                                <td>10 miles</td>
+                                <td>Medium</td>
+                                <td>Restrooms, Picnic Area</td>
+                                <td>4.5</td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
+            </div>
         </>
     );
 };
