2b7fe1867031e9d402b40d8016487a20baaf6a66
SamWatkins67 <118970009+SamWatkins67@users.noreply.github.com>
2024-05-09 08:48:32 -0700

    Update HomePage.jsx
    sam branch

--- client/src/views/HomePage.jsx
+++ client/src/views/HomePage.jsx
@@ -1,74 +1,79 @@
-import React, { useState, useEffect, useContext } from "react";
-import TopBanner from "../components/TopBanner";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 import { Link } from "react-router-dom";
-
-// imports for userContext
-import { userContext } from "../context/userContext";
-import { getUserById } from "../services/LoginService";
+import TopBanner from "../components/TopBanner";
+import Button from "react-bootstrap/Button";
+import Table from "react-bootstrap/Table";
 import { getAllHikes } from "../services/HikeService";
 
+
+
 const HomePage = () => {
-    // Variables to get logged in username
-    const { user, setUser } = useContext(userContext);
-    const id = window.localStorage.getItem("UUID");
-    const [erros, setErrors] = useState({});
+    const [hikes, setHikes] = useState([]);
 
-    // useState for the hike list
-    const [hikeList, setHikeList] = useState({});
-
-    // Get user username information
     useEffect(() => {
-        getUserById(id)
+        axios.get("http://localhost:9999/api/v1")
             .then((res) => {
-                console.log(res);
-                setUser(res);
-            })
-            .catch((err) => {
-                setErrors(err);
-            });
-
-        getAllHikes()
-            .then((res) => {
-                console.log(res);
-                setHikeList(res);
+                const hikesData = res.data;
+                setHikes(hikesData);
             })
             .catch((err) => {
                 console.log(err);
             });
     }, []);
 
+    const handleDelete = (id) => {
+        axios.delete(`http://localhost:9999/api/v1/${id}`)
+            .then((res) => {
+                console.log(res);
+                const remainingHikes = hikes.filter(hike => hike._id !== id);
+                setHikes(remainingHikes);
+            })
+            .catch((err) => {
+                console.log(err);
+            });
+    };
+
     return (
-        <>
-            <div className="full_screen_background">
-                <TopBanner loggedUser={user.username} />
-                <div className="container mt-5">
-                    <h1 className="mb-4">Hike Explorer</h1>
-                    <Link to="/">Go to Dev Page</Link>
-                    <table className="table table-bordered">
+        <div className="full_screen_background">
+            <TopBanner />
+            <div className="container mt-5">
+                <div className="card p-4 bg-light bg-opacity-50">
+                <h1 className="headersLabels">All Hikes</h1>
+                    <Table striped bordered>
                         <thead>
                             <tr>
                                 <th scope="col">Hike Name</th>
                                 <th scope="col">Location</th>
                                 <th scope="col">Distance</th>
                                 <th scope="col">Difficulty</th>
-                                <th scope="col">Facilities</th>
+                                <th scope="col">Amenities</th>
                                 <th scope="col">Rating</th>
+                                <th scope="col">Description</th>
+                                <th scope="col">Actions</th>
                             </tr>
                         </thead>
                         <tbody>
-                            <tr>
-                                <td>Example Hike</td>
-                                <td>Example Location</td>
-                                <td>10 miles</td>
-                                <td>Medium</td>
-                                <td>Restrooms, Picnic Area</td>
-                                <td>4.5</td>
-                            </tr>
+                            {hikes.map((hike) => (
+                                <tr key={hike._id}>
+                                    <td><Link to={`/hike/${hike._id}`}>{hike.hike_name}</Link></td>
+                                    <td>{hike.location}</td>
+                                    <td>{hike.distance} Miles</td>
+                                    <td>{hike.difficulty}</td>
+                                    <td>{Array.isArray(hike.amenities) ? hike.amenities.join(', ') : hike.amenities}</td>
+                                    <td>{hike.rating}</td>
+                                    <td>{hike.description}</td>
+                                    <td>
+                                        <Button onClick={() => handleDelete(hike._id)} variant="danger">Delete</Button>
+                                        <Link to={`/updatehike/${hike._id}`} className="btn btn-warning ms-2">Update</Link>
+                                    </td>
+                                </tr>
+                            ))}
                         </tbody>
-                    </table>
+                    </Table>
                 </div>
             </div>
-        </>
+        </div>
     );
 };
 
